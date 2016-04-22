ed58d318582a9374ccf4452d2328979eb42ef42c
gburnett <garyburnett3000@gmail.com>
2016-04-22 21:47:31 +0100

    Use functions

--- index.js
+++ index.js
@@ -1,85 +1,84 @@
 /* global window */
 //links
 //http://eloquentjavascript.net/09_regexp.html
-
-var messages = []; //array that hold the record of each string in chat
-var lastUserMessage = ""; //keeps track of the most recent input string from the user
-var botMessage = ""; //var keeps track of what the chatbot is going to say
-var botName = 'Kyran'; //name of the chatbot
-var UserName = 'User';
+var botName = 'Kyran';
 
 //edit this function to change what the chatbot says
-function chatbotResponse() {
+function chatbotResponse(userMessage) {
     'use strict';
-    botMessage = "Sorry. I don't know the answer yet."; //the default message
+    var defaultMessage = "Sorry. I don't know the answer yet.",
+        messages = {
+            'hi': 'Hi, How are you?',
+            'hello': 'Hi, How are you?',
+            'name': 'My name is ' + botName,
+            'what is the time?': 'look at the bottom right of your screen',
+            'claims': 'contact ***********',
+            'contact number': 'who would you like to contact?'
+        };
 
-    if (lastUserMessage === 'hi') {
-        botMessage = 'Hi, How are you?';
+    if (messages[userMessage] !== undefined) {
+        return messages[userMessage];
     }
+    return defaultMessage;
+}
 
-    if (lastUserMessage === 'hello') {
-        botMessage = 'Hi, How are you?';
-    }
+function getNameTag(name) {
+    'use strict';
+    var tag = window.document.createElement('b'),
+        boldText = name + ':',
+        text = window.document.createTextNode(boldText);
 
-    if (lastUserMessage === 'name') {
-        botMessage = 'My name is ' + botName;
-    }
+    tag.appendChild(text);
 
-    if (lastUserMessage === 'what is the time?') {
-        botMessage = 'look at the bottom right of your screen';
-    }
+    return tag;
+}
 
-    if (lastUserMessage === 'claims') {
-        botMessage = 'contact ***********';
-    }
+function getLogEntry(name, message) {
+    'use strict';
+    var logEntry = window.document.createElement('p'),
+        messageText = window.document.createTextNode(message),
+        nameTag = getNameTag(name);
 
-    if (lastUserMessage === 'contact number') {
-        botMessage = 'who would you like to contact?';
-    }
+    logEntry.appendChild(nameTag);
+    logEntry.appendChild(messageText);
+    logEntry.className = 'chatlog';
+
+    return logEntry;
 }
 
 //this runs each time enter is pressed.
 //It controls the overall input and output
-function newEntry() {
+function newEntry(entry) {
     'use strict';
-    var i;
-    //if the message from the user isn't empty then run
-    if (window.document.getElementById("chatbox").value !== "") {
-        //pulls the value from the chatbox ands sets it to lastUserMessage
-        lastUserMessage = window.document.getElementById("chatbox").value;
-        //sets the chat box to be clear
-        window.document.getElementById("chatbox").value = "";
-        window.document.getElementById("chatbox").placeholder = "";
-        //adds the value of the chatbox to the message array
-        messages.push("<b>" + UserName + ":</b> " + lastUserMessage);
-        //takes the return value from chatbotResponse() and outputs it
-        chatbotResponse();
-        //add the chatbot's name and message to the array messages
-        messages.push("<b>" + botName + ":</b> " + botMessage);
+    var userName = 'User',
+        userMessage = getLogEntry(userName, entry),
+        response = chatbotResponse(entry),
+        botMessage = getLogEntry(botName, response),
+        container = window.document.getElementById('chat-log');
 
-        //outputs the last few messages to html
-        for (i = 1; i < 8; i += 1) {
-            if (messages[messages.length - i]) {
-                window.document.getElementById("chatlog" + i).innerHTML = messages[messages.length - i];
-            }
-        }
-    }
+    container.appendChild(userMessage);
+    container.appendChild(botMessage);
+}
+
+function resetValue(target) {
+    'use strict';
+    target.value = '';
+    target.placeholder = '';
 }
 
 //if the key pressed is 'enter' runs the function newEntry()
 function keyPress(e) {
     'use strict';
-    var x = e || window.event;
-    var key = (x.keyCode || x.which);
+    var x = e || window.event,
+        key = (x.keyCode || x.which);
     if (key === 13 || key === 3) {
-        //runs this function when enter is pressed
-        newEntry();
-    }
-    if (key === 38) {
-        window.console.log('hi');
-        //window.document.getElementById("chatbox").value = lastUserMessage;
+        //if the message from the user isn't empty then run
+        if (x.target.value !== "") {
+            //runs this function when enter is pressed
+            newEntry(x.target.value);
+            resetValue(x.target);
+        }
     }
 }
-
 //runs the keypress() function when a key is pressed
 window.document.onkeypress = keyPress;
